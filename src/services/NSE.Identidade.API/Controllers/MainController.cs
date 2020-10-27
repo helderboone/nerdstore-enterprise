154c5a4c73cbabcc1c00d3ed82119189bd1b2818
Helder Boone <helder.boone@gmail.com>
2020-10-27 11:54:35 -0300

    Custom response

---
+++ src/services/NSE.Identidade.API/Controllers/MainController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore.Internal;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NSE.Identidade.API.Controllers
+{
+    [ApiController]
+    public abstract class MainController : Controller
+    {
+        protected ICollection<string> Errors = new List<string>();
+
+        protected ActionResult CustomResponse(object result = null)
+        {
+            if (OperacaoValida())
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(new ValidationProblemDetails(new Dictionary<string, string[]>
+            {
+                {"Mensagens", Errors.ToArray() }
+            }));
+        }
+
+        protected ActionResult CustomResponse(ModelStateDictionary modelState)
+        {
+            var errors = modelState.Values.SelectMany(e => e.Errors);
+
+            foreach (var erro in errors)
+            {
+                AdicionarErroProcessamento(erro.ErrorMessage);
+            }
+
+            return CustomResponse();
+        }
+
+        protected bool OperacaoValida() => !Errors.Any();
+
+        protected void AdicionarErroProcessamento(string erro)
+        {
+            Errors.Add(erro);
+        }
+
+        protected void LimparErrosProcessamento()
+        {
+            Errors.Clear();
+        }
+    }
+}
