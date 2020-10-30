f0024f3d7622d4b0bc441fa39306a693d9240977
Helder Boone <helder.boone@gmail.com>
2020-10-30 18:43:21 -0300

    Good practices http services part i

--- src/web/NSE.WebApp.MVC/Services/Service.cs
+++ src/web/NSE.WebApp.MVC/Services/Service.cs
@@ -1,10 +1,23 @@
 ﻿using NSE.WebApp.MVC.Extensions;
 using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace NSE.WebApp.MVC.Services
 {
     public abstract class Service
     {
+        protected StringContent ObterConteudo(object dado)
+        {
+            return new StringContent(JsonSerializer.Serialize(dado), Encoding.UTF8, "application/json");
+        }
+
+        protected async Task<T> DeserializarObjetoResponse<T>(HttpResponseMessage responseMessage)
+        {
+            return JsonSerializer.Deserialize<T>(await responseMessage.Content.ReadAsStringAsync(), new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+
         protected bool TratarErrosResponse(HttpResponseMessage response)
         {
             switch ((int) response.StatusCode)
