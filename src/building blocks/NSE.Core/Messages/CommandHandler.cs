0a6f7b855f2da06eca72611a1df072898513268a
Helder Boone <helder.boone@gmail.com>
2020-12-04 15:03:56 -0300

    MediatR e Command Handler

---
+++ src/building blocks/NSE.Core/Messages/CommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation.Results;
+
+namespace NSE.Core.Messages
+{
+    public abstract class CommandHandler
+    {
+        protected CommandHandler()
+        {
+            ValidationResult = new ValidationResult();
+        }
+
+        protected ValidationResult ValidationResult;
+
+        public void AdicionarErro(string mensagem)
+        {
+            ValidationResult.Errors.Add(new ValidationFailure(string.Empty, mensagem));
+        }
+    }
+}
--- src/services/NSE.Cliente.API/Configuration/DependecyInjectionConfig.cs
+++ src/services/NSE.Cliente.API/Configuration/DependecyInjectionConfig.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using NSE.Clientes.API.Data;
+﻿using FluentValidation.Results;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using NSE.Clientes.API.Application.Commands;
+using NSE.Core.Mediator;
 
 namespace NSE.Clientes.API.Configuration
 {
@@ -7,7 +10,8 @@ namespace NSE.Clientes.API.Configuration
     {
         public static void RegisterServices(this IServiceCollection services)
         {
-            services.AddScoped<ClientesContext>();
+            services.AddScoped<IMediatorHandler, MediatorHandler>();
+            services.AddScoped<IRequestHandler<RegistrarClienteCommand, ValidationResult>, ClienteCommandHandler>();
         }
     }
 }
