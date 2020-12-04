22d20fd2110e0b85a2ecc426c45a39263d865736
Helder Boone <helder.boone@gmail.com>
2020-12-04 14:11:06 -0300

    Command e Command Handler

---
+++ src/building blocks/NSE.Core/Messages/Command.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation.Results;
+using System;
+
+namespace NSE.Core.Messages
+{
+    public abstract class Command : Message
+    {
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+
+        public DateTime Timestamp { get; private set; }
+
+        public ValidationResult ValidationResult { get; set; }
+
+        public virtual bool EhValido()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
