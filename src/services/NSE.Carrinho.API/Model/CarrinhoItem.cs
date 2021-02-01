ae23960eff86b72c7b3be0a4d1160426d71608f6
Helder Boone <helder.boone@gmail.com>
2021-02-01 18:23:46 -0300

    Estrutura de dados da API de Carrinho

---
+++ src/services/NSE.Carrinho.API/Model/CarrinhoItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NSE.Carrinho.API.Model
+{
+    public class CarrinhoItem
+    {
+        public CarrinhoItem()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; private set; }
+        public Guid ProdutoId { get; private set; }
+        public string Nome { get; private set; }
+        public int Quantidade { get; private set; }
+        public decimal Valor { get; private set; }
+        public string Imagem { get; private set; }
+        public Guid CarrinhoId { get; private set; }
+        public CarrinhoCliente CarrinhoCliente { get; private set; }
+    }
+}
