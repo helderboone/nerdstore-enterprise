cab5c3f5cd2209c54b9e921d446146f3da4c2f47
Helder Boone <helder.boone@gmail.com>
2021-02-01 17:46:31 -0300

    Setup inicial do carrinho

---
+++ src/services/NSE.Carrinho.API/Configuration/DependecyInjectionConfig.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace NSE.Carrinho.API.Configuration
+{
+    public static class DependecyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            //services.AddScoped<CarrinhoContext>();
+        }
+    }
+}
