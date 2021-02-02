a95208bb6011bcefa53bf76e03caa63562ac2d66
Helder Boone <helder.boone@gmail.com>
2021-02-02 11:00:08 -0300

    Reconhecimento de usuario

--- src/services/NSE.Carrinho.API/Controllers/CarrinhoController.cs
+++ src/services/NSE.Carrinho.API/Controllers/CarrinhoController.cs
@@ -1,11 +1,46 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NSE.Carrinho.API.Model;
 using NSE.WebAPI.Core.Controllers;
+using NSE.WebAPI.Core.Usuario;
+using System;
+using System.Threading.Tasks;
 
 namespace NSE.Carrinho.API.Controllers
 {
     [Authorize]
     public class CarrinhoController : MainController
     {
-       
+        private readonly IAspNetUser _user;
+
+        public CarrinhoController(IAspNetUser user)
+        {
+            _user = user;
+        }
+
+        [HttpGet]
+       public async Task<CarrinhoCliente> ObterCarrinho()
+        {
+            return null;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AdicionarItemCarrinho(CarrinhoItem item)
+        {
+            return CustomResponse();
+        }
+
+        [HttpPut("carrinho/{produtoId}")]
+        public async Task<ActionResult> AtualizarItemCarrinho(Guid produtoId, CarrinhoItem item)
+        {
+            return CustomResponse();
+        }
+
+        [HttpDelete("carrinho/{produtoId}")]
+        public async Task<ActionResult> RemoverItemCarrinho(Guid produtoId)
+        {
+            return CustomResponse();
+        }
+
     }
 }
