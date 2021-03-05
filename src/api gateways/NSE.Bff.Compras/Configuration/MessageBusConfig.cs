eecf0a8bec9e2926e2ecd3902dd9dfbe242d96ea
Helder Boone <helder.boone@gmail.com>
2021-03-05 18:31:13 -0300

    Implementando BFF de Compras

---
+++ src/api gateways/NSE.Bff.Compras/Configuration/MessageBusConfig.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NSE.Bff.Compras.Configuration
+{
+    public static class MessageBusConfig
+    {
+        public static void AddMesageBusConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+
+        }
+    }
+}
---
+++ src/api gateways/NSE.Bff.Compras/Services/Service.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace NSE.Bff.Compras.Services
+{
+    public abstract class Service
+    {
+        protected StringContent ObterConteudo(object dado)
+        {
+            return new StringContent(JsonSerializer.Serialize(dado), Encoding.UTF8, "application/json");
+        }
+
+        protected async Task<T> DeserializarObjetoResponse<T>(HttpResponseMessage responseMessage)
+        {
+            return JsonSerializer.Deserialize<T>(await responseMessage.Content.ReadAsStringAsync(), new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+
+        protected bool TratarErrosResponse(HttpResponseMessage response)
+        {
+            if (response.StatusCode == HttpStatusCode.BadRequest) return false;
+
+            response.EnsureSuccessStatusCode();
+            return true;
+        }
+    }
+}
