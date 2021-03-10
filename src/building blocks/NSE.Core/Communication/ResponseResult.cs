be2acc8f8a9f6a25e2f160d3b2ea124599ce4b7a
Helder Boone <helder.boone@gmail.com>
2021-03-10 20:20:58 -0300

    Consumindo servicos pelo BFF pt 1

---
+++ src/building blocks/NSE.Core/Communication/ResponseResult.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace NSE.Core.Communication
+{
+    public class ResponseResult
+    {
+        public ResponseResult()
+        {
+            Errors = new ResponseErrorMessage();
+        }
+
+        public string Title { get; set; }
+        public int Status { get; set; }
+        public ResponseErrorMessage Errors { get; set; }
+    }
+
+    public class ResponseErrorMessage
+    {
+        public ResponseErrorMessage()
+        {
+            Mensagens = new List<string>();
+        }
+
+        public List<string> Mensagens { get; set; }
+    }
+}
