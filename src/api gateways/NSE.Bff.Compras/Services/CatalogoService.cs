c533636d3865f0d6d88d958ff06b3e44da385482
Helder Boone <helder.boone@gmail.com>
2021-03-15 18:38:27 -0300

    Consumindo servicos pelo BFF pt 2

--- src/api gateways/NSE.Bff.Compras/Services/CatalogoService.cs
+++ src/api gateways/NSE.Bff.Compras/Services/CatalogoService.cs
@@ -1,11 +1,17 @@
 ﻿using Microsoft.Extensions.Options;
 using NSE.Bff.Compras.Extensions;
+using NSE.Bff.Compras.Models;
 using System;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace NSE.Bff.Compras.Services
 {
-    public interface ICatalogoService { }
+    public interface ICatalogoService 
+    {
+        Task<ItemProdutoDTO> ObterPorId(Guid id);
+    }
+
     public class CatalogoService : Service, ICatalogoService
     {
         private readonly HttpClient _httpClient;
@@ -14,6 +20,15 @@ namespace NSE.Bff.Compras.Services
         {
             _httpClient = httpClient;
             _httpClient.BaseAddress = new Uri(settings.Value.CatalogoUrl);
+        }
+
+        public async Task<ItemProdutoDTO> ObterPorId(Guid id)
+        {
+            var response = await _httpClient.GetAsync($"/catalogo/produtos/{id}");
+
+            TratarErrosResponse(response);
+
+            return await DeserializarObjetoResponse<ItemProdutoDTO>(response);
         }
     }
 }
