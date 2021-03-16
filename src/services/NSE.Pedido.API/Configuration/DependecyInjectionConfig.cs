93cbf4ee562e1b335906f0a071a3cf6552c93692
Helder Boone <helder.boone@gmail.com>
2021-03-16 14:08:50 -0300

    Definindo arquitetura da API de Pedidos

---
+++ src/services/NSE.Pedido.API/Configuration/DependecyInjectionConfig.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using NSE.Pedidos.Infra.Data;
+using NSE.WebAPI.Core.Usuario;
+
+namespace NSE.Pedidos.API.Configuration
+{
+    public static class DependecyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<IAspNetUser, AspNetUser>();
+            services.AddScoped<PedidosContext>();
+        }
+    }
+}
