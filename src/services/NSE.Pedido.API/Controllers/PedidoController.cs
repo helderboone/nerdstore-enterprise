55f76663177f497ceb79c92f03a7ebfae36486fe
Helder Boone <helder.boone@gmail.com>
2021-04-09 18:52:16 -0300

    Finalizando a API de Pedido

---
+++ src/services/NSE.Pedido.API/Controllers/PedidoController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSE.Core.Mediator;
+using NSE.Pedidos.API.Application.Commands;
+using NSE.Pedidos.API.Application.Queries;
+using NSE.WebAPI.Core.Controllers;
+using NSE.WebAPI.Core.Usuario;
+using System.Threading.Tasks;
+
+namespace NSE.Pedidos.API.Controllers
+{
+    public class PedidoController : MainController
+    {
+        private readonly IMediatorHandler _mediator;
+        private readonly IAspNetUser _user;
+        private readonly IPedidoQueries _pedidoQueries;
+
+        public PedidoController(IMediatorHandler mediator, IAspNetUser user, IPedidoQueries pedidoQueries)
+        {
+            _mediator = mediator;
+            _user = user;
+            _pedidoQueries = pedidoQueries;
+        }
+
+        [HttpPost("pedido")]
+        public async Task<IActionResult> AdicionarPedido(AdicionarPedidoCommand pedido)
+        {
+            pedido.ClienteId = _user.ObterUserdId();
+            return CustomResponse(await _mediator.EnviarComando(pedido));
+        }
+
+        [HttpGet("pedido/ultimo")]
+        public async Task<IActionResult> UltimoPedido()
+        {
+            var pedido = await _pedidoQueries.ObterUltimoPedido(_user.ObterUserdId());
+
+            return pedido == null ? NotFound() : CustomResponse(pedido);
+        }
+
+        [HttpGet("pedido/lista-cliente")]
+        public async Task<IActionResult> ListaPorCliente()
+        {
+            var pedidos = await _pedidoQueries.ObterListaPorClienteId(_user.ObterUserdId());
+
+            return pedidos == null ? NotFound() : CustomResponse(pedidos);
+        }
+    }
+}
