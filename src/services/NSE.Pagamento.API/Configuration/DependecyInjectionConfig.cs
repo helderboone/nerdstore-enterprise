339289939a9c668dae2e31b5565dd1d32cf873d0
Helder Boone <helder.boone@gmail.com>
2021-05-03 19:19:17 -0300

    Solicitando processamento de pagamento

---
+++ src/services/NSE.Pagamento.API/Configuration/DependecyInjectionConfig.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using NSE.WebAPI.Core.Usuario;
+
+namespace NSE.Pagamento.API.Configuration
+{
+    public static class DependecyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            //API
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<IAspNetUser, AspNetUser>();
+
+        }
+    }
+}
