959a0abba9ac8ba5b7bba34b7cc5a16125f866d7
Helder Boone <helder.boone@gmail.com>
2021-05-05 20:14:49 -0300

    Background service de pagamento

---
+++ src/services/NSE.Pagamento.API/Services/PagamentoService.cs
@@ -0,0 +1,51 @@
+﻿using FluentValidation.Results;
+using NSE.Core.Messages.Integration;
+using NSE.Pagamentos.API.Facade;
+using NSE.Pagamentos.API.Models;
+using System.Threading.Tasks;
+
+namespace NSE.Pagamentos.API.Services
+{
+    public class PagamentoService : IPagamentoService
+    {
+        private readonly IPagamentoFacade _pagamentoFacade;
+        private readonly IPagamentoRepository _pagamentoRepository;
+
+        public PagamentoService(IPagamentoFacade pagamentoFacade, IPagamentoRepository pagamentoRepository)
+        {
+            _pagamentoFacade = pagamentoFacade;
+            _pagamentoRepository = pagamentoRepository;
+        }
+
+        public async Task<ResponseMessage> AutorizarPagamento(Pagamento pagamento)
+        {
+            var transacao = await _pagamentoFacade.AutorizarPagamento(pagamento);
+
+            var validationResult = new ValidationResult();
+
+            if (transacao.Status != StatusTransacao.Autorizado)
+            {
+                validationResult.Errors.Add(new ValidationFailure("Pagamento", "Pagamento recusado, entre em contato com a sua operadora"));
+
+                return new ResponseMessage(validationResult);
+            }
+
+            pagamento.AdicionarTransacao(transacao);
+
+            _pagamentoRepository.AdicionarPagamento(pagamento);
+
+            if (!await _pagamentoRepository.UnitOfWork.Commit())
+            {
+                validationResult.Errors.Add(new ValidationFailure("Pagamento", "Houve um erro ao realizar o pagamento"));
+
+                //TODO: Comunicar com o gateway para realizar o estorno
+                // Poderia enviar uma mensage Pub/Sub Estornar valor do pedido caso outras API estejam interessadas na msg. 
+                //Ou chamar um metodo diretamente de estorno da Facade
+
+                return new ResponseMessage(validationResult);
+            }
+
+            return new ResponseMessage(validationResult);
+        }
+    }
+}
