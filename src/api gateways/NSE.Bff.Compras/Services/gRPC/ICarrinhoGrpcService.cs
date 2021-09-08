fe4099671c2e01b130ee088f0ee3ee8ddff1444c
helderboone <helder.boone@gmail.com>
2021-09-08 20:22:00 -0300

    Implementando um gRPC Client

---
+++ src/api gateways/NSE.Bff.Compras/Services/gRPC/ICarrinhoGrpcService.cs
@@ -0,0 +1,10 @@
+﻿using NSE.Bff.Compras.Models;
+using System.Threading.Tasks;
+
+namespace NSE.Bff.Compras.Services.gRPC
+{
+    public interface ICarrinhoGrpcService
+    {
+        Task<CarrinhoDTO> ObterCarrinho();
+    }
+}
